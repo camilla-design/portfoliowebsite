81c83a6b07eed5d8f74b197157067ccb58279acb
Atlungstad Brenneri <camilla_repp@hotmail.com>
2023-03-28 02:39:21 +0200

    header
    Header started

---
+++ pages/index.tsx
@@ -0,0 +1,28 @@
+import type { NextPage } from "next";
+import Head from 'next/head';
+import Header from "../components/Header";
+
+
+const Home: NextPage = ()  => {
+  return (
+    <>
+      <Head>
+        <title>Portfolio Website</title>
+       
+      </Head>
+
+     {/** Header */}
+     <Header />
+     
+     {/** Hero */}
+
+     {/** About */}
+
+     {/** Projekter */}
+
+     {/** Contakt */}
+    </>
+  );
+};
+
+export default Home;
